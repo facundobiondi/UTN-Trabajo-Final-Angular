f196c154e672b93226da932212e91be760c8ced9
«Facu» <facu_biondi@hotmail.com>
2024-07-05 18:58:37 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, RouterLink, HomeComponent],
+  template: `
+    <main>
+      <header class="brand-mame">
+        <h1 routerLink="/">Encabezado</h1>
+        <img src="./assets/logo.jpg" alt="Logo" />
+      </header>
+
+      <section class="content">
+        <router-outlet></router-outlet>
+      </section>
+    </main>
+  `,
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'ecommerce';
-}
+export class AppComponent {}
